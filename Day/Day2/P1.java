b694f9f0dbf62a00921322c5588cea13281e5519
RakeshdotKR <rakeshkrpdr@gmail.com>
2026-09-21 18:22:24 +0530

    file renamed

---
+++ Day/Day2/P1.java
@@ -0,0 +1,25 @@
+// Demonstrates a generic class that can safely store different reference types.
+class Box<T> {
+    private T value;
+
+    public void set(T value) {
+        this.value = value;
+    }
+
+    public T get() {
+        return value;
+    }
+}
+
+public class P1 {
+    public static void main(String[] args) {
+        Box<String> stringBox = new Box<>();
+        stringBox.set("Hello");
+
+        Box<Integer> integerBox = new Box<>();
+        integerBox.set(42);
+
+        System.out.println(stringBox.get());
+        System.out.println(integerBox.get());
+    }
+}
